1c0f5297ce9e7957c440856cbcbea52ce92c74a0
sumeetbidhan <sumeetbidhanwork@gmail.com>
2024-05-08 14:26:39 +0530

    changes to pdf and css

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -3,37 +3,37 @@ const Services_Data = [
         s_no:"01",
         s_name:"Data Analysis and Insights",
         s_desc:"Transforming raw data into actionable insights through in-depth analysis, statistical modeling, and data visualization techniques, empowering informed decision-making and strategic planning.",
-        pdf_link:"src/assets/1.pdf"
+        pdf_link:"public/1.pdf"
     },
     {
         s_no:"02",
         s_name:"Predictive Modeling and Forecasting",
         s_desc:"Building robust predictive models to forecast trends, identify patterns, and uncover hidden opportunities, enabling proactive strategies and mitigating risks.",
-        pdf_link:"src/assets/2.pdf"
+        pdf_link:"public/2.pdf"
     },
     {
         s_no:"03",
         s_name:"Database Management and Optimization",
         s_desc:"Managing and optimizing databases, designing efficient data schemas, writing optimized SQL queries, and implementing performance tuning strategies for enhanced data storage and retrieval.",
-        pdf_link:"src/assets/3.pdf"
+        pdf_link:"public/3.pdf"
     },
     {
         s_no:"04",
         s_name:"Data-Driven Web Applications",
         s_desc:"Designing and developing interactive web applications powered by data analysis, incorporating robust back-end functionalities and intuitive front-end interfaces for seamless user experiences.",
-        pdf_link:"src/assets/4.pdf"
+        pdf_link:"public/4.pdf"
     },
     {
         s_no:"05",
         s_name:"Custom Data APIs and Integrations",
         s_desc:"Developing custom APIs and data integrations to connect web applications with external data sources, enabling seamless data exchange and synchronization for enhanced functionality.",
-        pdf_link:"src/assets/5.pdf"
+        pdf_link:"public/5.pdf"
     },
     {
         s_no:"06",
         s_name:"Full Stack Consulting and Solution Architecture",
         s_desc:"Providing strategic consulting on full stack architecture, technology stack selection, and solution design, ensuring alignment with business goals, scalability, and performance optimization.",
-        pdf_link:"src/assets/6.pdf"
+        pdf_link:"public/6.pdf"
     },
 ]
  
